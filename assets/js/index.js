af8972c352b9505d910a4f71fda19a65e3dd7d54
val8119 <mikk.vahaste@gmail.com>
2020-06-27 00:36:41 +0300

    Added loading tasks from localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,8 @@ addButton.addEventListener("click", addTask);
 
 tasksList.addEventListener("click", checkDelCompl);
 
+document.addEventListener("DOMContentLoaded", getTasks);
+
 // functions
 function addTask(event) {
     event.preventDefault(); // stop refresh
@@ -20,6 +22,9 @@ function addTask(event) {
     const taskItemLi = document.createElement("li");
     taskItemLi.innerText = addInput.value;
     taskItemLi.classList.add("task-text");
+
+    // save task name to local storage
+    saveTasks(addInput.value);
 
     // add list item to div
     taskItemDiv.appendChild(taskItemLi);
@@ -48,26 +53,29 @@ function addTask(event) {
 }
 
 function checkDelCompl(event) {
-    const target = event.target;
+    const task = event.target;
 
     // check if clicked on delete button
-    if (target.classList[0] === "task-del") {
-        target.parentElement.classList.toggle("deleted");
+    if (task.classList[0] === "task-del") {
+        task.parentElement.classList.toggle("deleted");
+
+        // deletes task from local storage
+        delStorageTask(task);
 
         // check if animation has ended
-        target.parentElement.addEventListener("transitionend", function () {
-            target.parentElement.remove();
+        task.parentElement.addEventListener("transitionend", function () {
+            task.parentElement.remove();
         });
     }
 
     // check if clicked on completed button
-    if (target.classList[0] === "task-compl") {
-        target.parentElement.classList.toggle("completed");
+    if (task.classList[0] === "task-compl") {
+        task.parentElement.classList.toggle("completed");
     }
 }
 
 function saveTasks(task) {
-    // check
+    // check if local storage is empty
     let tasks;
     if (localStorage.getItem("tasks") === null) {
         tasks = [];
@@ -75,8 +83,72 @@ function saveTasks(task) {
         tasks = JSON.parse(localStorage.getItem("tasks"));
     }
 
+    // append to tasks array
     tasks.push(task);
 
+    // save array to local storage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getTasks() {
+    // check if local storage is empty
+    let tasks = [];
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function (task) {
+        // create task item div
+        const taskItemDiv = document.createElement("div");
+        taskItemDiv.classList.add("task");
+
+        // create task item li
+        const taskItemLi = document.createElement("li");
+        taskItemLi.innerText = task;
+        taskItemLi.classList.add("task-text");
+
+        // add list item to div
+        taskItemDiv.appendChild(taskItemLi);
+
+        // create completed button
+        const taskCompletedButton = document.createElement("button");
+        taskCompletedButton.innerHTML = '<i class="fas fa-check"></i>';
+        taskCompletedButton.classList.add("task-compl");
+
+        // add button to div
+        taskItemDiv.appendChild(taskCompletedButton);
+
+        // create delete button
+        const taskDeleteButton = document.createElement("button");
+        taskDeleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+        taskDeleteButton.classList.add("task-del");
+
+        // add button to div
+        taskItemDiv.appendChild(taskDeleteButton);
+
+        // insert div into document
+        tasksList.appendChild(taskItemDiv);
+    });
+}
+
+function delStorageTask(task) {
+    // check if local storage is empty
+    let tasks = [];
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    // get the index of the task
+    const taskIndex = task.parentElement.children[0].innerText;
+
+    // remove item at the task index
+    tasks.splice(tasks.indexOf(taskIndex), 1);
+
+    // save the new tasks array
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
